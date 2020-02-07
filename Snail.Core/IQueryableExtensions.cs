d74f0d2ae16100628eb0da0cf3a470df640d80bc
zhoujing <zhoujing@rlair.net>
2020-02-07 10:30:53 +0800

    增加queryable自定义字段排序

--- Snail.Core/IQueryableExtensions.cs
+++ Snail.Core/IQueryableExtensions.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Snail.Common
@@ -37,5 +38,34 @@ namespace Snail.Common
             }
         }
 
+        public static IQueryable<TSource> Order<TSource>(this IQueryable<TSource> query, IQueryOrderBy queryOrderBy)
+        {
+            if (string.IsNullOrEmpty(queryOrderBy.OrderByField))
+            {
+                return query;
+            }
+            var prop = typeof(TSource).GetProperties().ToList().FirstOrDefault(a => a.Name.Equals(queryOrderBy.OrderByField, StringComparison.OrdinalIgnoreCase));
+            if (prop == null)
+            {
+                return query;
+            }
+            var orderMethodName = queryOrderBy.OrderBySort == ESort.Asc ? "OrderBy" : "OrderByDescending";
+            var param = Expression.Parameter(typeof(TSource));
+            var types = new List<Type> { typeof(TSource), prop.PropertyType };
+            Expression expr = Expression.Call(typeof(Queryable), orderMethodName, types.ToArray(), query.Expression, Expression.Lambda(Expression.Property(param, prop.Name), param));
+            query = query.AsQueryable().Provider.CreateQuery<TSource>(expr);
+            return query;
+        }
+    }
+
+    public interface IQueryOrderBy
+    {
+        string OrderByField { get; set; }
+        ESort? OrderBySort { set; get; }
+    }
+    public enum ESort
+    {
+        Asc,
+        Des
     }
 }
---
+++ Snail.Core/Interface/IExcelHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Snail.Core.Interface
+{
+    public interface IExcelHelper
+    {
+        List<T> ImportFromExcel<T>(Stream fileStream);
+        Stream ExportToExcel<T>(List<T> data);
+    }
+}
