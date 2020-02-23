6b9457b68fc7fcf37215362e8a92ea57d8e7267b
周晶 <373087455@qq.com>
2020-02-23 22:59:26 +0800

    2020

---
+++ Snail.Common/Extenssions/EnumExtenssion.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snail.Common.Extenssions
+{
+    public static class EnumExtenssion
+    {
+    }
+}
--- updatePackage.bat
+++ updatePackage.bat
@@ -1,11 +1,11 @@
 ## 参考 https://docs.microsoft.com/zh-cn/dotnet/core/tools/dotnet-nuget-push
 echo 正在打包...
-dotnet pack .\Snail.Core\Snail.Core.csproj -o .\NugetPackage -p:PackageVersion=1.0.3
-dotnet pack .\Snail.Core.Default\Snail.Core.Default.csproj -o .\NugetPackage -p:PackageVersion=1.0.3
+dotnet pack .\Snail.Core\Snail.Core.csproj -o .\NugetPackage -p:PackageVersion=1.0.6
+dotnet pack .\Snail.Core.Default\Snail.Core.Default.csproj -o .\NugetPackage -p:PackageVersion=1.0.6
 dotnet pack .\Snail.Common\Snail.Common.csproj -o .\NugetPackage -p:PackageVersion=1.0.3
-dotnet pack .\Snail.DAL\Snail.DAL.csproj -o .\NugetPackage -p:PackageVersion=1.0.3
-dotnet pack .\Snail.FileStore\Snail.FileStore.csproj -o .\NugetPackage -p:PackageVersion=1.0.3
-dotnet pack .\Snail.Office\Snail.Office.csproj -o .\NugetPackage -p:PackageVersion=1.0.2
-dotnet pack .\Permission\Snail.Permission.csproj -o .\NugetPackage -p:PackageVersion=1.0.3
-dotnet nuget push .\NugetPackage\*.nupkg -k oy2j7wk52md6qzngbzxqoitnybyaurve5iqxseukzuykgq -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet pack .\Snail.DAL\Snail.DAL.csproj -o .\NugetPackage -p:PackageVersion=1.0.5
+dotnet pack .\Snail.FileStore\Snail.FileStore.csproj -o .\NugetPackage -p:PackageVersion=1.0.5
+dotnet pack .\Snail.Office\Snail.Office.csproj -o .\NugetPackage -p:PackageVersion=1.0.5
+dotnet pack .\Permission\Snail.Permission.csproj -o .\NugetPackage -p:PackageVersion=1.0.5
+dotnet nuget push .\NugetPackage\*.nupkg -k --oy2m--qpf6dpnsfq55--jnpq2qspjgd3nr--sunlm2qcj3hj--an6a -s https://api.nuget.org/v3/index.json --skip-duplicate
 pause
