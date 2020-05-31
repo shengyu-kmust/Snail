e95919712e67a7804ce7457544d332376da9ad01
周晶 <373087455@qq.com>
2020-05-31 11:50:35 +0800

    修改entity的createtime为可空

--- Snail.Common/DateTimeHelper.cs
+++ Snail.Common/DateTimeHelper.cs
@@ -6,6 +6,16 @@ namespace Snail.Common
 {
     public class DateTimeHelper
     {
-       
+        public DateTime? ConvertFrom(string date)
+        {
+            if (DateTime.TryParse(date,out DateTime dateTime))
+            {
+                return dateTime;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
