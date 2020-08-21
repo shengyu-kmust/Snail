6e2e695872b62db2d1775f9f073b73a2d256efc5
zhoujing <zhoujing@rlair.net>
2020-08-21 17:08:53 +0800

    增加pay项目

---
+++ Snail.Pay/Controllers/PayController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Snail.Pay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PayController : ControllerBase
+    {
+        [HttpGet,HttpPost]
+        public object Notify()
+        {
+            return null;
+        }
+
+        [HttpGet,HttpPost]
+        public object ToPay()
+        {
+            return null;
+        }
+    }
+}
