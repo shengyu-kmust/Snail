96213ea9f2d6a69e7bc34255e410b226e069225a
zhoujing <zhoujing@rlair.net>
2020-12-28 15:16:26 +0800

    snail.weixin增加获取accesstoken方法

---
+++ Snail.WeiXin/Models/AccessTokenResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snail.WeiXin.Models
+{
+    /// <summary>
+    /// accessToken返回结构
+    /// </summary>
+    /// <remarks>
+    /// https请求方式: GET https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
+    /// </remarks>
+    public class AccessTokenResult: WxJsonResult
+    {
+        /// <summary>
+        /// 获取到的凭证
+        /// </summary>
+        public string access_token { get; set; }
+        /// <summary>
+        /// 凭证有效时间，单位：秒
+        /// </summary>
+        public int expires_in { get; set; }
+    }
+}
